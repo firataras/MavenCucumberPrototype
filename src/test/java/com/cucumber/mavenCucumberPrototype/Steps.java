4980960f7b7d55abc90b2c2f582b36443787aef9
Firat Aras <firat.aras@argela.com.tr>
2016-06-02 11:06:40 +0300

    Second Commit

--- src/test/java/com/cucumber/mavenCucumberPrototype/Steps.java
+++ src/test/java/com/cucumber/mavenCucumberPrototype/Steps.java
@@ -7,19 +7,19 @@ import cucumber.api.java.en.When;
 public class Steps {
 	
 	@Given("^This is my first dummy given step$")
-	public void this_is_my_first_dummy_given_step() throws Throwable {
+	public void firststep() throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
 	    System.out.println("This is my first dummy GIVEN step");
 	}
 
 	@When("^This is my second when step$")
-	public void this_is_my_second_when_step() throws Throwable {
+	public void secondstep() throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
 		System.out.println("This is my second  dummy WHEN step");
 	}
 
 	@Then("^This is my third then step$")
-	public void this_is_my_third_then_step() throws Throwable {
+	public void thirdstep() throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
 		System.out.println("This is my third dummy THEN step");
 	}
